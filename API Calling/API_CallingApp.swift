bc4fd5a6c4239756af1c6f1b75b95eec704315fa
tyler0611 <berlity0611@gmail.com>
2025-03-04 09:31:44 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Tyler Berlin on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
